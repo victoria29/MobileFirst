1db5a4e9fccbbfa672b317c8d69bf33e0bb9b8b2
Вика <Vmartynova99@gmail.com>
2020-10-10 11:25:44 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="prev"></button>',
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+
+                }
+            },
+
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false
+                }
+            }
+
+        ]
+    });
+});
