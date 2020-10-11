278fa3a6dae6ee1ef876f40da48bbea5270d4eb7
Вика <Vmartynova99@gmail.com>
2020-10-11 13:09:12 +0300

    finish commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ $(function(){
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next"></button>',
-        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="assets/images/next-arrow.svg" alt="next"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="assets/images/prev-arrow.svg" alt="prev"></button>',
 
         responsive: [
             {
